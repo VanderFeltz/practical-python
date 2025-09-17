625178aa52c09c1c463463b4ed279390901e3b27
Will VanderFeltz <wrv@mac.lan>
2025-09-16 22:38:11 -0400

    continued progress on Practical Python

--- Work/bounce.py
+++ Work/bounce.py
@@ -5,7 +5,7 @@
 height = 100 # meters
 bounce = 1
 
-for bounce in range(11):
+for bounce in range(1,11):
     height = height * 0.6
     print(bounce, round(height, 4))
     bounce += 1
--- Work/mortgage.py
+++ Work/mortgage.py
@@ -1,3 +1,27 @@
 # mortgage.py
 #
 # Exercise 1.7
+# mortgage.py
+
+principal = 500000.0
+rate = 0.05
+payment = 2684.11
+total_paid = 0.0
+num_months = 0
+extra_payment_start_month = 61
+extra_payment_end_month = 108
+extra_payment = 1000
+
+while principal > 0:
+    num_months +=1
+    principal = principal * (1+rate/12) - payment
+    total_paid = total_paid + payment
+
+    if num_months >= extra_payment_start_month and num_months <= extra_payment_end_month:
+        principal = principal - extra_payment
+        total_paid = total_paid + extra_payment
+
+    print(num_months, round(total_paid, 2), round(principal, 2))
+
+print('Total paid', round(total_paid, 2))
+print('Months:', num_months)
